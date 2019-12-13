e8519b6dd84b7f71f4ddfe58e29125272111d333
George Mihov <gmihov001@gmail.com>
2019-12-13 14:46:00 +0000

    Initial commit

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -3,8 +3,16 @@ import React, { Component } from "react";
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+			Created <i className="fas fa-copyright text-secondary" /> by George Mihov
 		</p>
+		<a href="#" target="_blank">
+			<i className="fab fa-facebook-square" />
+		</a>
+		<a href="#" target="_blank">
+			<i className="fab fa-twitter-square" />
+		</a>
+		<a href="#" target="_blank">
+			<i className="fab fa-instagram" />
+		</a>
 	</footer>
 );
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,14 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
+		<h1>Welcome to my page!</h1>
 		<p>
-			<img src={rigoImage} />
+			<img src="#" />
 		</p>
 		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
+			Hello
 		</a>
 	</div>
 );
