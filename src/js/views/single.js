48655042f5f8aadd3307f17aab874285d26e5c9e
George Mihov <gmihov001@gmail.com>
2020-01-31 05:26:15 +0000

    Updates

--- src/js/views/single.js
+++ src/js/views/single.js
@@ -8,9 +8,7 @@ export const Single = props => {
 	return (
 		<div className="jumbotron">
 			<h1 className="display-4">This will show the demo element: {store.demo[props.match.params.theid].title}</h1>
-
 			<hr className="my-4" />
-
 			<Link to="/">
 				<span className="btn btn-primary btn-lg" href="#" role="button">
 					Back home
