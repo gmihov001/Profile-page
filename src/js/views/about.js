e1f9000be2d8515377a20928cfc8627b265c8ae3
George Mihov <gmihov001@gmail.com>
2020-02-08 01:57:44 +0000

    updates

--- src/js/views/about.js
+++ src/js/views/about.js
@@ -9,6 +9,18 @@ export const About = () => {
 				<div className="row row1 about py-5 d-flex justify-content-end">
 					<span className="h3 section-heading"> ABOUT ME </span>
 				</div>
+				<div className="row row2 py-5 d-flex justify-content-end">
+					<p className="h5"> Hi, my name is George and I am a web developer </p>
+					<p className="h5"> My hobies are coding, coding and coding. </p>
+					<p className="h5">
+						{" "}
+						Before I was a developer I was once an immigrant who one day decided to pursue his dreams in the
+						United States, and moved here with one suitcase and $227. In the United States I put myself in
+						college and made a corporate career for myself. Why am I telling you this? To tell you that if
+						you need someone who believes that everything is possible and &quot;can&lsquo;t&quot; is not a
+						real word, then I may be your guy. :){" "}
+					</p>
+				</div>
 			</div>
 		</div>
 	);
