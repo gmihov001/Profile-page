49225239633b86f1cc53f467e2ea830e651f9d13
George Mihov <gmihov001@gmail.com>
2020-02-13 17:27:14 +0000

    Updates

--- src/js/component/proj1.js
+++ src/js/component/proj1.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
-import Wandetracker from "../../img/wandertracker.jpg";
+import Wandertracker1 from "../../img/wandertracker1.jpg";
 
 export const Project1 = () => {
 	return (
@@ -10,9 +10,21 @@ export const Project1 = () => {
 				<div className="row portfolio ml-0 py-5 d-flex justify-content-end">
 					<span className="h3 section-heading">Wandertracker</span>
 				</div>
-				<div className="row secondary py-5 d-flex justify-content-around">
-					<div className="col #1 border border-secondary"> Column1 </div>
-					<div className="col #2 border border-secondary"> Column2 </div>
+				<div className="row secondary p-5 d-flex justify-content-around">
+					<div className="col #1 m-3 border border-secondary">
+						{" "}
+						Column1
+						<img className="img-thumbnail" id="#" src={Wandertracker1} />
+						<img className="img-thumbnail" id="#" src={Wandertracker1} />
+						<img className="img-thumbnail" id="#" src={Wandertracker1} />
+						<img className="img-thumbnail" id="#" src={Wandertracker1} />
+						<img className="img-thumbnail" id="#" src={Wandertracker1} />
+						<img className="img-thumbnail" id="#" src={Wandertracker1} />
+						<img className="img-thumbnail" id="#" src={Wandertracker1} />
+						<img className="img-thumbnail" id="#" src={Wandertracker1} />
+					</div>
+
+					<div className="col #2 m-3 border border-secondary"> Column2 </div>
 				</div>
 			</div>
 		</div>
