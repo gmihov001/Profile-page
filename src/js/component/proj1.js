3ef6799506e99a01a81f545f8486e8e7507374cc
George Mihov <gmihov001@gmail.com>
2020-02-13 16:02:03 +0000

    Updates

--- src/js/component/proj1.js
+++ src/js/component/proj1.js
@@ -8,11 +8,12 @@ export const Project1 = () => {
 		<div className="wrapper">
 			<div className="container-fluid px-0">
 				<div className="row portfolio ml-0 py-5 d-flex justify-content-end">
-					<span className="h3 section-heading">Project 1.0</span>
+					<span className="h3 section-heading">Wandertracker</span>
 				</div>
 				<div className="row secondary py-5">
 					<div className="row m-auto">
-						<div className="column #1"></div>
+						<div className="column #1 bg-secondary"></div>
+						<div className="column #2 bg-secondary"></div>
 					</div>
 				</div>
 			</div>
